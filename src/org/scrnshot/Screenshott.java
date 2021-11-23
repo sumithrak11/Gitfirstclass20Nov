11ae5b46cf0c2de0cccc4ffbc1b0de1c93119505
sumithrak11 <sumithrak11@gmail.com>
2021-11-23 08:17:15 +0800

    testpurpose

---
+++ src/org/scrnshot/Screenshott.java
@@ -0,0 +1,27 @@
+package org.scrnshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshott {
+public static void main(String[] args) throws IOException {
+	System.setProperty("webdriver.chrome.driver", "C:\\Sumi\\Eclipse\\Screenshottt\\ChromeDriver\\chromedriver.exe");
+	
+	WebDriver driver=new ChromeDriver();
+	
+	driver.get("https://www.snapdeal.com/");
+	driver.manage().window().maximize();
+	
+	
+	
+	
+}
+}
